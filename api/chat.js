2c764eedc9741c3501a101e13514b8905277b615
Stathis Drim <asosproject@outlook.com>
2026-03-18 11:37:44 +0200

    Change export syntax from default to module.exports

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     // Τραβάει το κλειδί από το Vercel Environment Variables
     const API_KEY = process.env.GEMINI_KEY; 
     
