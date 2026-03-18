e03ed83d0f8a084f12e1fb2f6b3ae278a9edaa04
Stathis Drim <asosproject@outlook.com>
2026-03-18 13:53:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         userInput.value = '';
 
         try {
-            const response = await fetch(URL, {
+            const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDc9cwD46Jc-C0cK9ZVlgg8b1pgBHGjzG0`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
